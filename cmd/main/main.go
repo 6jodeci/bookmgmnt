16f651cf6f7fa66c152ac138045d096ce1788c66
6jodeci <egor0263s@gmail.com>
2022-04-30 21:59:15 +0500

    1/2

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,16 @@
-// 1.35.54
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"library/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,18 +1,23 @@
 package config
 
 import (
-        "gorm.io/driver/mysql"
-        "gorm.io/gorm"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 var (
-    db  *gorm.DB
+	dsn = "root:769321djR@tcp(127.0.0.1:3306)/bookmgmnt?charset=utf8mb4&parseTime=True&loc=Local"
+	db  *gorm.DB
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:769321djR@tcp(127.0.0.1:3306)/bookmgmnt?charset=utf8mb4&parseTime=True&loc=Local")
+	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db = d
 }
 
 func GetDB() *gorm.DB {
-        return db
+	return db
 }
